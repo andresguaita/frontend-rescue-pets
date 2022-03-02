ebb27f1e0e32e3b4ebc42d1e1190530d915a72e6
Julian-Lop <you@example.com>
2022-03-02 13:46:16 -0500

    Front-and-add-styles-createForm-julian

--- src/Styles/StyledDashboardForms.js
+++ src/Styles/StyledDashboardForms.js
@@ -18,6 +18,10 @@ export const StyledDashboardForms = styled.div`
     font-size: 25px;
     width: 200px;
     border-radius: 10px;
+    &:hover{
+      background-color : ${allColors.colors[2]};
+      color: rgba(255,255,255,0.8);
+    }
   }
   
   table {
