e97883074a9ab499f0749901d57d3197b9a5d146
Andres Guaita <andresguaita300@gmail.com>
2022-03-06 14:54:21 -0500

    se agrego el inicio de sesion con google

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -3,7 +3,7 @@ import Swal from 'sweetalert2';
 import { getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import { googleAuthProvider } from '../../firebase/firebase-Config'
 import { fetchConToken, fetchSinToken } from '../../helpers/fetch.js';
-import {cloudynary} from '../../helpers/cloudinary.js'
+import { cloudynary } from '../../helpers/cloudinary.js'
 import store from '../Store/index';
 import {
     GET_COUNTRIES,
@@ -76,7 +76,7 @@ import {
     GET_PET_HIDE_FOR_ADMIN,
     GET_SHELTER_OF_PET_FOR_ADMIN,
     ADD_FOLLOW_UP_TRANSIT
-    } from './types.js'
+} from './types.js'
 import { async } from '@firebase/util';
 import { APIGATEWAY_URL } from '../../utils/constant';
 
@@ -107,7 +107,7 @@ export const getPets = () => {
 
 export const getCountries = () => {
     return async function (dispatch) {
-      
+
         let json = await axios(`${APIGATEWAY_URL}/country`);
         return dispatch({ type: GET_COUNTRIES, payload: json.data });
     };
@@ -258,7 +258,7 @@ export const startRegister = (name, phoneNumber, description, address, email, pa
             Swal.fire('Genial', 'Registro realizado correctamente', 'success')
         }
 
-        else{
+        else {
             Swal.fire('Error', 'Algo salio mal, por favor intentelo nuevamente', 'error')
 
             Swal.fire('Genial', 'Informacion actualizada', 'success')
@@ -394,21 +394,21 @@ export const startChecking = () => {
     return async (dispatch) => {
         const resp = await fetchConToken('renew')
         const body = await resp.json()
-        
+
         if (body.ok) {
-           
+
             localStorage.setItem('token', body.token)
             localStorage.setItem('token-init-date', new Date().getTime())
-            if(body.role==3 || body.rol==2){
+            if (body.role == 3 || body.rol == 2) {
                 dispatch(loginAdmin({ id: body.id, email: body.email, rol: body.role }));
             }
 
-            else{
-                dispatch(login({ id: body.id, email: body.email}));
+            else {
+                dispatch(login({ id: body.id, email: body.email }));
             }
         }
         else {
-            
+
             dispatch(checkingFinish())
         }
     }
@@ -421,8 +421,33 @@ export const startGoogleLogin = () => {
         const auth = getAuth()
         signInWithPopup(auth, googleAuthProvider)
             .then(({ user }) => {
-                dispatch(login())
+                dispatch(checkUser(user.email))
             })
+    }
+}
+
+const checkUser = (email) => {
+    return async (dispatch) => {
+
+        try {
+            const resp = await fetchSinToken("loginGoogle", {
+                email,
+            }, "POST");
+            const body = await resp.json()
+
+            if (body.ok) {
+                localStorage.setItem("token", body.token);
+                localStorage.setItem("token-init-date", new Date().getTime());
+                dispatch(login({ id: body.id, email: body.email }));
+            } else {
+
+                Swal.fire('Error', body.msg, 'error')
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
 
@@ -432,7 +457,7 @@ export const login = (user) => ({
 })
 
 export function postPets(payload) {
-    console.log("console desde accion  ",payload)
+    console.log("console desde accion  ", payload)
     return async function () {
         const post = await axios.post(`${APIGATEWAY_URL}/pets`, payload);
         return post;
@@ -592,16 +617,16 @@ export const postCreateForm = (form) => {
 }
 
 
-export const getFormByShelter = (shelterid,formtypeid) => {
-    return async function(dispatch){
+export const getFormByShelter = (shelterid, formtypeid) => {
+    return async function (dispatch) {
         let json = await axios(`${APIGATEWAY_URL}/getFormByShelter/${shelterid}?formtypeid=${formtypeid}`)
-        return dispatch({type:GET_FORM_BY_SHELTER, payload:json.data})
+        return dispatch({ type: GET_FORM_BY_SHELTER, payload: json.data })
     }
 }
 
-export const editForm = (formid,formtypeid,questions) => {
-    return async function(dispatch){
-        let json = await axios.put(`${APIGATEWAY_URL}/editForm/${formid}?formtypeid=${formtypeid}`,questions)
+export const editForm = (formid, formtypeid, questions) => {
+    return async function (dispatch) {
+        let json = await axios.put(`${APIGATEWAY_URL}/editForm/${formid}?formtypeid=${formtypeid}`, questions)
         return json
     }
 }
@@ -631,7 +656,7 @@ export const uploadImageCloud = (formData) => {
         const body = await resp.data.secure_url
         return body
     };
-    
+
 }
 
 
@@ -646,44 +671,44 @@ export const addFollowUp = (payload) => {
 
 export const getFollowUpsFromShelter = (shelterId) => {
     return async function (dispatch) {
-        const followUps= await axios.get(`${APIGATEWAY_URL}/getFollowUps/${shelterId}`);
-        return dispatch({ type: GET_FOLLOW_UPS_FROM_SHELTER, payload:followUps.data });
+        const followUps = await axios.get(`${APIGATEWAY_URL}/getFollowUps/${shelterId}`);
+        return dispatch({ type: GET_FOLLOW_UPS_FROM_SHELTER, payload: followUps.data });
     };
 }
 
 export const sendEmailAccepted = (payload) => {
-    return async function(dispatch){
-        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailAccepted`,payload)
+    return async function (dispatch) {
+        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailAccepted`, payload)
         return json
     }
 }
 
 export const sendEmailRejected = (payload) => {
-    return async function(dispatch){
-        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailRejected`,payload)
+    return async function (dispatch) {
+        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailRejected`, payload)
         return json
     }
 }
 
 export const checkForm = (shelterid) => {
-    return async function(dispatch){
+    return async function (dispatch) {
         let json = await axios(`${APIGATEWAY_URL}/checkForm/${shelterid}`)
-        return dispatch({type:CHECK_FORM, payload:json.data})
+        return dispatch({ type: CHECK_FORM, payload: json.data })
     }
 
 }
 
 export const sendEmailForms = (payload) => {
-    return async function(){
-        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailForms`,payload)
+    return async function () {
+        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailForms`, payload)
         return json
     }
 }
 
 
 export const sendEmailFormstoShelter = (payload) => {
-    return async function(){
-        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailFormstoShelter`,payload)
+    return async function () {
+        let json = await axios.post(`${APIGATEWAY_URL}/nodemailer/sendEmailFormstoShelter`, payload)
         return json
     }
 }
@@ -695,17 +720,17 @@ export const postHelpSupport = (payload) => {
             "Genial!",
             "Registro realizado correctamente. Pronto nos comunicaremos contigo",
             "sucess"
-          );
-        
+        );
+
     }
-    
+
 };
 
 
 export const addFollowUpTransit = (data) => {
     return async function (dispatch) {
-        const followUpTransit= await axios.post(`${APIGATEWAY_URL}/addFollowUpTransit`);
-        return dispatch({ type: ADD_FOLLOW_UP_TRANSIT, payload:data });
+        const followUpTransit = await axios.post(`${APIGATEWAY_URL}/addFollowUpTransit`);
+        return dispatch({ type: ADD_FOLLOW_UP_TRANSIT, payload: data });
     };
 }
 
@@ -726,27 +751,27 @@ export const addFollowUpTransit = (data) => {
 
 
 export const ModalDashboardOpen = (modal) => {
-    return {type: MODAL_DASHBOARD, payload: modal};
+    return { type: MODAL_DASHBOARD, payload: modal };
 };
 
 export const getProfile = (profileId) => {
-    return async function(dispatch){
+    return async function (dispatch) {
         let json = await axios(`${APIGATEWAY_URL}/profiles/${profileId}`)
-        return dispatch({type:GET_PROFILE, payload: json.data})
+        return dispatch({ type: GET_PROFILE, payload: json.data })
     }
 }
 
-export const StartRestorePassword =  (email) => {
+export const StartRestorePassword = (email) => {
     return async (dispatch) => {
 
         try {
             const resp = await fetchSinToken(`forgotpassword/?email=${email}`);
-            const body= await resp.json()
-            
+            const body = await resp.json()
+
             if (body.ok) {
                 Swal.fire('Restaurar Contraseña', 'Revise su email, le hemos enviado un enlace para restablecer su contraseña', 'info')
             } else {
-               
+
                 Swal.fire('Error', body.msg, 'error')
             }
 
@@ -758,7 +783,7 @@ export const StartRestorePassword =  (email) => {
 
 };
 
-export const resetPassword = (token, password) =>{
+export const resetPassword = (token, password) => {
     return async (dispatch) => {
         const resp = await fetchSinToken("resetpassword", {
             token,
@@ -775,7 +800,7 @@ export const resetPassword = (token, password) =>{
 
 export const getFollowUpStatuses = () => {
     return async function (dispatch) {
-        const followUpStatuses= await axios.get(`${APIGATEWAY_URL}/followUpStatuses`);
+        const followUpStatuses = await axios.get(`${APIGATEWAY_URL}/followUpStatuses`);
         return dispatch({ type: GET_FOLLOW_UPS_STATUSES, payload: followUpStatuses.data });
     };
 }
@@ -838,23 +863,23 @@ export const getCountAdopted2 = () => {
 export function addToFavorites(pet) {
     const jsonPet = JSON.stringify(pet);
     localStorage.setItem(pet.id, jsonPet);
-  
+
     return { type: ADD_TO_FAVORITES, payload: pet };
-  }
-  
+}
+
 export function removeFromFavorites(pet) {
     localStorage.removeItem(pet.id);
-  
+
     return {
-      type: REMOVE_FROM_FAVORITES,
-      payload: pet.id,
+        type: REMOVE_FROM_FAVORITES,
+        payload: pet.id,
     };
-  }
+}
 
-  
 
-export const setFormStatus = (status,formid,id) => {
-    return async function(dispatch){
+
+export const setFormStatus = (status, formid, id) => {
+    return async function (dispatch) {
         let json = await axios.put(`${APIGATEWAY_URL}/setFormStatus/${id}/${formid}/${status}`)
     }
 }
@@ -863,14 +888,14 @@ export const setFormStatus = (status,formid,id) => {
 export const hidePetInDashbaord = (petId, payload) => {
     return async function (dispatch) {
         const hidePetInDashbaord = await axios.put(`${APIGATEWAY_URL}/pets/hide/${petId}`, payload);
-        return dispatch({ type: EDIT_HIDE_PETS_IN_DASHBOARD, payload:hidePetInDashbaord });
+        return dispatch({ type: EDIT_HIDE_PETS_IN_DASHBOARD, payload: hidePetInDashbaord });
         // console.log(editPet)
         // return editPet
 
     };
 }
 
-export const createAdmin= (email,password,roleId,userRole) =>{
+export const createAdmin = (email, password, roleId, userRole) => {
     return async (dispatch) => {
         const resp = await fetchSinToken("createAdmin", {
             email,
@@ -879,7 +904,7 @@ export const createAdmin= (email,password,roleId,userRole) =>{
             userRole
         }, "POST");
         const body = await resp.json();
-      
+
         if (body.ok) {
             alert(body.msg);
         } else {
@@ -891,8 +916,8 @@ export const createAdmin= (email,password,roleId,userRole) =>{
 
 export const updatePetStatus = (petId, payload) => {
     return async function (dispatch) {
-        const updatePetStatus = await axios.put(`${APIGATEWAY_URL}/pets/updateStatus/${petId}`, payload); 
-        return dispatch({ type: EDIT_PET_STAUTS_ID, payload:updatePetStatus });
+        const updatePetStatus = await axios.put(`${APIGATEWAY_URL}/pets/updateStatus/${petId}`, payload);
+        return dispatch({ type: EDIT_PET_STAUTS_ID, payload: updatePetStatus });
 
     };
 }
@@ -901,22 +926,22 @@ export const updatePetStatus = (petId, payload) => {
 
 
 
-export const getAllShelters= () =>{
+export const getAllShelters = () => {
 
     return async (dispatch) => {
         const resp = await fetchSinToken("getAllShelter");
         const body = await resp.json();
         if (body.ok) {
-          dispatch(allShelters(body.shelters))
+            dispatch(allShelters(body.shelters))
         } else {
             alert(body.msg);
         }
     };
 }
 
-const allShelters = (shelters) =>({type: GET_ALL_SHELTERS, payload: shelters})
+const allShelters = (shelters) => ({ type: GET_ALL_SHELTERS, payload: shelters })
 
-export const editShelterByAdmin = (id, email, status) =>{
+export const editShelterByAdmin = (id, email, status) => {
 
     return async (dispatch) => {
         const resp = await fetchSinToken("editShelter", {
@@ -937,22 +962,26 @@ export const editShelterByAdmin = (id, email, status) =>{
 export const hideFollowUpfromDash = (followUpId, payload) => {
     return async function (dispatch) {
         const hideFollowUp = await axios.put(`${APIGATEWAY_URL}/hideFollowUp/${followUpId}`, payload);
-        return dispatch({ type: EDIT_HIDE_PETS_IN_DASHBOARD, payload:hideFollowUp });
+        return dispatch({ type: EDIT_HIDE_PETS_IN_DASHBOARD, payload: hideFollowUp });
     };
 }
 
 
 export const getOnlyCitiesWithShelter = () => {
-    return async function (dispatch){
-        return dispatch({type:GET_ONLY_CITIES_WITH_SHELTERS,
-                        payload: null})
+    return async function (dispatch) {
+        return dispatch({
+            type: GET_ONLY_CITIES_WITH_SHELTERS,
+            payload: null
+        })
     }
 }
 
 export const getOnlyStatesWithShelter = () => {
-    return async function (dispatch){
-        return dispatch({type:GET_ONLY_STATES_WITH_SHELTERS,
-                        payload: null})
+    return async function (dispatch) {
+        return dispatch({
+            type: GET_ONLY_STATES_WITH_SHELTERS,
+            payload: null
+        })
     }
 }
 
@@ -969,23 +998,23 @@ export const getPetsFilterForAdmin = (link) => {
 
 
 export const getStatusForAdmin = () => {
-    return { type: GET_STATUS_FOR_ADMIN, payload: null}
+    return { type: GET_STATUS_FOR_ADMIN, payload: null }
 };
 
 
 export const getSpeciesForAdmin = () => {
-    return {type: GET_SPECIES_FOR_ADMIN, payload: null}
+    return { type: GET_SPECIES_FOR_ADMIN, payload: null }
 }
 
 export const getGenresForAdmin = () => {
-    return {type: GET_PET_GENRE_FOR_ADMIN, payload:null}
+    return { type: GET_PET_GENRE_FOR_ADMIN, payload: null }
 }
 
 export const getHideForAdmin = () => {
-    return {type: GET_PET_HIDE_FOR_ADMIN, payload:null}
+    return { type: GET_PET_HIDE_FOR_ADMIN, payload: null }
 }
 
 export const getShelterOfPetForAdmin = () => {
-    return {type: GET_SHELTER_OF_PET_FOR_ADMIN, payload:null}
+    return { type: GET_SHELTER_OF_PET_FOR_ADMIN, payload: null }
 
 }
