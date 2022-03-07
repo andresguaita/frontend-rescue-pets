06f5cd34a0c4157bf51f0b4524ca67f681a10352
JRClaros <jose.ronald.claros@gmail.com>
2022-03-07 11:38:13 -0400

    Actualización de Grafico del Statistics

--- src/components/GraphicStatisChelter.jsx
+++ src/components/GraphicStatisChelter.jsx
@@ -63,24 +63,18 @@ const estado=auxDatos
         datasets:[
             {
             label:"Cantidad Adoptados por Especies",
-            backgroundColor:' #63ac44',
+            backgroundColor:[
+                "#EE6C4D",
+                "#F38D68",
+                "#662C91",
+                "#17A398",
+                "#33312E"
+            ],
             borderColor:'black',
             borderWidth:1,
             hoverBackgroundColor:'rgba(0,255,0,0.2)',
             hoverBorderColor:'#FFFF00',
             data:estado
-            // fill: false,
-            // backgroundColor: 'rgba(73,155,234,1)',
-            // borderColor:'rgba(73,155,234,1)',
-            // pointBorderColor:'rgba(73,155,234,1)',
-            // pointBorderWidth:1,
-            // pointHoverRadius:5,
-            // pointHoverBackgroundColor:'rgba(73,155,234,1)',
-            // pointHoverBorderColor:'rgba(73,155,234,1)',
-            // pointRadius: 1,
-            // pointHitRadius: 10,
-            // data: [0.17, 19, 156, 357, 565, 1149]
-            
             }
         ]
     }
