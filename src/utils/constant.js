f9b7809d5d53edd94ee1d6db9dffaf32f866b901
Luis Abanto <lfam200@hotmail.com>
2022-03-08 16:41:20 -0500

    Constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const APIGATEWAY_URL = "https://rescuet-pet-backend.herokuapp.com"
-// export const APIGATEWAY_URL = "http://localhost:3001"
+// export const APIGATEWAY_URL = "https://rescuet-pet-backend.herokuapp.com"
+export const APIGATEWAY_URL = "http://localhost:3001";
